8774b44ba6ffa96fed54b18e0ff21443cc11d394
tiagoGottardo <tiago.panizio@hotmail.com>
2025-08-23 19:19:05 -0300

    refactor: remove oauth

--- api/src/http/mod.rs
+++ api/src/http/mod.rs
@@ -3,17 +3,15 @@ use anyhow::Context;
 use auth::register;
 use axum::{Router, routing::post};
 use error::Error;
-use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl, basic::BasicClient};
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 use std::{
-    collections::HashMap,
     net::{Ipv4Addr, SocketAddr},
-    sync::Arc,
     time::Duration,
 };
 use time::OffsetDateTime;
-use tokio::{net::TcpListener, sync::RwLock};
+use tokio::net::TcpListener;
 use tower_http::{
     catch_panic::CatchPanicLayer, compression::CompressionLayer, timeout::TimeoutLayer,
     trace::TraceLayer,
@@ -29,10 +27,7 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 pub struct AppState {
     pub config: Config,
     pub db: PgPool,
-    // pub oauth_client: BasicClient<EndpointSet, EndpointNotSet, EndpointNotSet, EndpointNotSet, EndpointSet>,
-    pub device_codes: Arc<RwLock<HashMap<String, u64>>>,
-    // TODO: Add reqwest::client here.
-    // pub oauth_states: Arc<tokio::sync::RwLock<HashMap<String, String>>>,
+    client: Client, // TODO: Add reqwest::client here.
 }
 
 #[derive(Debug)]
@@ -49,32 +44,14 @@ pub struct GitHubUser {
     pub email: Option<String>,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct AuthRequest {
-    pub code: String,
-    pub state: String,
-}
-
 pub async fn serve(config: Config, db: PgPool) -> anyhow::Result<()> {
-    let github_client_id = ClientId::new(config.github_client_id.to_owned());
-    let github_client_secret = ClientSecret::new(config.github_client_secret.to_owned());
-    let auth_url = AuthUrl::new("https://github.com/login/oauth/authorize".to_string())?;
-    let token_url = TokenUrl::new("https://github.com/login/oauth/access_token".to_string())?;
-
-    let oauth_client = BasicClient::new(github_client_id)
-        .set_client_secret(github_client_secret)
-        .set_auth_uri(auth_url)
-        .set_token_uri(token_url)
-        .set_redirect_uri(RedirectUrl::new(format!(
-            "http://0.0.0.0:{}/auth/github/callback",
-            &config.port
-        ))?);
-
     let app_state = AppState {
         config: config.clone(),
+        client: Client::builder()
+            .user_agent("CAIROS/1.0.0")
+            .build()
+            .expect("Error on build Client."),
         db,
-        // oauth_client,
-        device_codes: Arc::new(tokio::sync::RwLock::new(HashMap::new())),
     };
 
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, config.port));
