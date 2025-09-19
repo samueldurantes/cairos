95589e44cdba7c031f36d0b68a0043d38b070e5a
Tiago Gottardo <50255813+tiagoGottardo@users.noreply.github.com>
2025-09-19 19:44:45 -0300

    fix: create config file if missing (#9)

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -1,5 +1,10 @@
 use serde::{Deserialize, Serialize};
-use std::{env, path::PathBuf};
+use std::{
+    env,
+    fs::{self, File},
+    io,
+    path::PathBuf,
+};
 
 #[derive(Deserialize, Serialize)]
 pub struct Config {
@@ -9,8 +14,9 @@ pub struct Config {
 
 impl Config {
     pub fn load() -> Self {
-        let config_file_dir = get_config_file_path().expect("Config directory not found");
-        let config_content = std::fs::read_to_string(config_file_dir).expect("Run: cairos setup");
+        let config_file_path = get_config_file_path().expect("Error on get config file path");
+        let config_content = get_or_create_config_content(config_file_path)
+            .expect("Error on get config file content");
         let config = toml::from_str::<Config>(&config_content).expect("Config file is weird");
 
         Self {
@@ -18,6 +24,27 @@ impl Config {
             token: config.token,
         }
     }
+}
+
+pub fn get_or_create_config_content(config_file_path: PathBuf) -> io::Result<String> {
+    if !config_file_path.exists() {
+        config_file_path
+            .parent()
+            .map(|parent| fs::create_dir_all(parent));
+
+        File::create(&config_file_path)?;
+
+        let my_config = Config {
+            base_url: "https://localhost".to_owned(),
+            token: None,
+        };
+
+        let toml_content = toml::to_string_pretty(&my_config).expect("Toml serialization failed");
+
+        fs::write(&config_file_path, toml_content)?;
+    }
+
+    std::fs::read_to_string(config_file_path)
 }
 
 pub fn get_config_file_path() -> Option<PathBuf> {
